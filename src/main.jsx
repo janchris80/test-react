3a2e34305c67cb0bf1fcc6533c3b89092e280cee
jcodev <admin@jcodev.online>
2026-05-30 01:04:19 +0000

    feat: update greeting to test auto-deploy via webhook

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <h1>Hello from React (deployed via deployer)</h1>
+    <h1>Hello from React (auto-deploy) (deployed via deployer)</h1>
   </React.StrictMode>
 );
